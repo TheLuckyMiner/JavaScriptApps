2969f3fa4b8b312d8d2d68e92db7e4db20a7b174
TheLuckyMiner <121936425+TheLuckyMiner@users.noreply.github.com>
2023-03-15 12:17:51 +0300

    Create Passe the message project

---
+++ ConsoleReader/script.js
@@ -0,0 +1,12 @@
+var words = ["hello","choose","you","second"];
+var answer = [];
+
+for (var i =0; i < words.length; i++){
+    answerArray[i] = '_';
+}
+
+var remainLetters = word.length;
+
+while(remainLetters > 0){
+    alert(answer.join(" "));
+}
