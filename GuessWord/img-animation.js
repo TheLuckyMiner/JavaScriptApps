e4b407dd4c2d7f32613c6d07c60ebeeaf8f5f292
TheLuckyMiner <121936425+TheLuckyMiner@users.noreply.github.com>
2023-03-18 11:25:45 +0300

    add image change function

---
+++ GuessWord/img-animation.js
@@ -0,0 +1,41 @@
+let word = ['a','b','c','d']; let string = '';
+for (let i = 0; i < word.length; i++) {string += word[i]}
+document.querySelector('.guessWord').textContent = string;
+let errorAmount = 0;
+let doesntFound = 1;
+
+const imgLinks = [
+    'images/error0.png',
+    'images/error1.png',
+    'images/error2.png',
+    'images/error3.png',
+    'images/error4.png',
+    'images/error5.png',
+    'images/error6.png',
+    'images/error7.png'
+]
+
+
+function getValue(){
+    let text = document.getElementById("input_form").value;
+    text = text[0];
+    doesntFound = 1;
+
+    for(let i =0; i < word.length; i++){
+        if(word[i] === text){
+            console.log('Found!', word[i]);
+            doesntFound = 0;
+        }
+    }
+    if(doesntFound){
+        errorAmount++; 
+        console.log(errorAmount);
+        if (errorAmount > 7){
+            alert("Вы проиграли! Загаданное слово: " + string);
+            errorAmount = 0;
+        };
+        document.getElementById('image').src = imgLinks[errorAmount]
+        
+    }
+    
+}
--- GuessWord/index.js
+++ GuessWord/index.js
@@ -29,3 +29,29 @@ while(remainingLetters > 0){
 
 alert(answerArray.join(" "));
 alert("Отлично! Было загадано слово: " + word);
+
+
+/*
+let page = document.querySelector('.page');
+let themeButton = document.querySelector('.theme-button');
+themeButton.onclick = function() {
+  page.classList.toggle('light-theme');
+  page.classList.toggle('dark-theme');
+};
+
+let message = document.querySelector('.subscription-message');
+
+let form = document.querySelector('.subscription');
+form.onsubmit = function(evt) {
+  // Инструкция ниже отменяет отправку данных
+  evt.preventDefault();
+  message.textContent = 'Форма отправлена!';
+};
+
+form.onsubmit = function(evt) {
+  evt.preventDefault();
+  // Измените значение textContent на следующей строке
+  message.textContent = 'Адрес ' + email.value + ' добавлен в список получателей рассылки.';
+};
+
+*/
