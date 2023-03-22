6a08de11ce3d037d3d6dd5be3f17eda26b900bd3
TheLuckyMiner <121936425+TheLuckyMiner@users.noreply.github.com>
2023-03-22 08:37:21 +0300

    add treasure page

--- Animation/script.js
+++ Animation/script.js
@@ -1,3 +1,67 @@
+let leftCoord = 0;
+let topCoord = 0; 
+
+let maxWidth = 200;
+let gameSpeed = 10;
+
+let moveHeading = function() {
+    $('p').offset({left: leftCoord, top: topCoord});
+    if (leftCoord < maxWidth && topCoord === 0){
+        leftCoord++;
+    } else
+    if (leftCoord === maxWidth && topCoord < maxWidth){
+        topCoord++;
+    } else
+    if (topCoord === maxWidth && leftCoord > 0){
+        leftCoord--;
+    } else
+    if (leftCoord === 0 && topCoord > 0){
+        topCoord--;
+    }
+}
+
+let finish = setInterval(moveHeading, gameSpeed);
+
+/*
+let moveHeading = function() {
+    $('p').offset ({left: leftCoord, top: topCoord});
+
+    if (lastDirection === 'top'){
+        leftCoord++;
+    } else if (lastDirection === 'right'){
+
+    }
+
+    leftOffSet++;
+    if (leftOffSet > 200) {
+        leftOffSet = 0;
+    }
+}
+setInterval(moveHeading, 30);
+
+
+
+let clickHandler = function (event){
+    $('p').offset( { 
+        left: event.pageX,
+        top: event.pageY
+    });
+}
+$('html').click(clickHandler);
+
+$('html').mousemove(function (event){
+    $('p').offset( { 
+        left: event.pageX,
+        top: event.pageY
+    });
+});
+
+let clickHandler = function (event) {
+    console.log("Click!" + event.pageX + ' ' + event.pageY);
+};
+
+$('p').click(clickHandler);
+
 let leftOffSet = 0;
 let moveHeading = function(){
     $('#list').offset({left: leftOffSet});
@@ -9,4 +73,4 @@ let moveHeading = function(){
     }
 };
 
-setInterval(moveHeading, 3000);
+setInterval(moveHeading, 3000);*/
---
+++ Treasure/script.js
@@ -0,0 +1,49 @@
+let getRandomNumber = function (size){
+    return Math.floor(Math.random() * size);
+    clicks = 0;
+};
+
+let getDistance = function (event, target){
+    let diffX = event.offsetX - target.x;
+    let diffY = event.offsetY - target.y;
+    return Math.sqrt((diffX * diffX) + (diffY * diffY));
+};
+
+let getDistanceHint = function (distance){
+    if (distance < 10) {
+        return "Обожжешься!";
+    } else if (distance < 20){
+        return "Очень горячо!";
+    } else if (distance < 40){
+        return "Горячо!";
+    } else if (distance < 80){
+        return "Тепло!";
+    } else if (distance < 160){
+        return "Холодно!";
+    } else if (distance < 320){
+        return 'Очень холодно!';
+    } else {
+        return 'Замерзнешь!';
+    }
+};
+
+let width = 833;
+let height = 500;
+let clicks = 0;
+
+let target = {
+    x: getRandomNumber(width),
+    y: getRandomNumber(height)
+};
+
+$('.map').click(function (event){
+    clicks++;
+
+    let distance = getDistance(event, target);
+    let distanceHint = getDistanceHint(distance);
+    $('.distance').text(distanceHint);
+
+    if(distance < 8){
+        alert("Клад найден! Сделано кликов: " + clicks);
+    }
+});
