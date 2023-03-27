e65b2219f44ccac87a02fcdaeb4b4a27430b0fe8
TheLuckyMiner <121936425+TheLuckyMiner@users.noreply.github.com>
2023-03-27 15:02:28 +0300

    bugfix arrow

--- QuestionList/index.js
+++ QuestionList/index.js
@@ -1,4 +1,8 @@
 function displayHint(clicked_id){
-    let hint_id = '#' + 'hint' + clicked_id.slice(-1);
+    let id = clicked_id.slice(-1);
+    let hint_id = '#hint' + id;
+    let arrow_id = '#arrow' +  id;
+    document.querySelector(arrow_id).classList.toggle('hint__show-arrow');
     document.querySelector(hint_id).classList.toggle('hint__show');
+
 }
